4ed70ced6b10977937051b6333e9957c568967ed
Marwan Alani <marwan@alani.ca>
2017-01-27 14:01:59 -0500

    Added solution for Funny String from hackerrank.com

---
+++ misc/FunnyString.swift
@@ -0,0 +1,36 @@
+//
+//  FunnyString.swift
+//  
+//  Practice solution - Marwan Alani - 2017
+//
+//  Check the problem (and run the code) on HackerRank @ https://www.hackerrank.com/challenges/funny-string
+//
+
+if let cases = Int(readLine() ?? "0") {
+    var output = [String]()
+    
+    // Process Input
+    for _ in 1...cases {
+        if let line: String = readLine() {
+            var funny = true
+            let myString = line.utf16
+            let count = myString.count
+            for offset in 0...(count/2) - 1 {
+                let firstFrontAscii = Int(myString[myString.index(myString.startIndex, offsetBy: offset)])
+                let secondFrontAscii = Int(myString[myString.index(myString.startIndex, offsetBy: offset+1)])
+                let firstBackAscii = Int(myString[myString.index(myString.startIndex, offsetBy: (count-1) - offset)])
+                let secondBackAscii = Int(myString[myString.index(myString.startIndex, offsetBy: (count-2) - offset)])
+                    if (abs(firstFrontAscii - secondFrontAscii) != abs(firstBackAscii - secondBackAscii)) {
+                    funny = false;
+                    break
+                }
+            }
+            output.append(funny ? "Funny" : "Not Funny")
+        }
+    }
+    
+    // Print Output
+    for line in output {
+        print(line)
+    }
+}
