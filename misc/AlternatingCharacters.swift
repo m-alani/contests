da958d7dc8752593289c9f0026850e7b32aa7b23
Marwan Alani <marwan@alani.ca>
2017-01-28 15:05:27 -0500

    Added initial solution to Alternating Characters

---
+++ misc/AlternatingCharacters.swift
@@ -0,0 +1,33 @@
+if let cases: Int = Int(readLine() ?? "0") {
+    var output: [String] = [String]()
+
+    // Process Input
+    for _ in 1...cases {
+        if let input: String = readLine() {
+            // Initializations
+            var line: String.CharacterView = input.characters
+            var deleteOperations: Int = 0
+            var charIndex: String.CharacterView.Index = line.startIndex
+            var previousChar: Character = line[charIndex]
+            charIndex = line.index(after: charIndex)
+            // Loop through the input line, starting at the second character
+            while (charIndex != line.endIndex) {
+                // Same character as before? Delete
+                if (line[charIndex] == previousChar) {
+                    line.remove(at: charIndex)
+                    deleteOperations += 1
+                } else {
+                    // Not the same? update the previous character variable, and move on
+                    previousChar = line[charIndex]
+                    charIndex = line.index(after: charIndex)
+                }
+            }
+            output.append("\(deleteOperations)")
+        }
+    }
+
+    // Print Output
+    for line in output {
+        print(line)
+    }
+}
