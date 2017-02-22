a21ee660179f9a62dd11e101351de6bb67653100
Marwan Alani <marwan@alani.ca>
2017-02-21 22:37:41 -0500

    Added solution for HackerRank - Sherlock and Array

---
+++ hackerrank/SherlockAndArray.swift
@@ -0,0 +1,30 @@
+
+import Foundation
+
+// Reading the number of test cases
+let cases = Int(readLine() ?? "0") ?? 0
+
+// Processing the test cases
+for _ in 0..<cases {
+  var output = "NO"
+  var left = 0
+  // The number of elements in the array doesn't matter in our implementation, but we need to clear that line from the input buffer
+  let caseLength = Int(readLine() ?? "0") ?? 0
+  // Read the test case into an array of strings (Swift readline() limitation)
+  let inputString = String(readLine() ?? "")!.components(separatedBy: " ")
+  // Convert the strings into an array of integers
+  let input = inputString.map({Int($0) ?? 0})
+  // Find the sum of the array
+  let sum = input.reduce(0, {$0 + $1})
+  // Process the array to find the requested index where the sum of its left is equal to the sum of its right (if any)
+  for number in input {
+    if ((sum-number)/2 == left) {
+      output = "YES"
+      break
+    } else {
+      left += number
+    }
+  }
+  // Print the output for this test case
+  print(output)
+}
