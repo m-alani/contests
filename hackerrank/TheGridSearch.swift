e45149df1732e92a6541525a9b4ccbe265dd626e
Marwan Alani <marwan@alani.ca>
2017-03-10 16:10:08 -0500

    Added solution for HackerRank - The Grid Search in Swift

---
+++ hackerrank/TheGridSearch.swift
@@ -0,0 +1,72 @@
+//
+//  TheGridSearch.swift
+//
+//  Practice solution - Marwan Alani - 2017
+//
+//  Check the problem (and run the code) on HackerRank @ https://www.hackerrank.com/challenges/the-grid-search
+//  Note: make sure that you select "Swift" from the top-right language menu of the code editor when testing this code
+//
+
+import Foundation
+
+// Helper function to check if a matrix is contained in another matrix at specific coordenates
+func does(thisMatrix main: [[Int]], containThisOne sub: [[Int]], atThisRow x: Int, andColumn y: Int) -> Bool {
+  for checkX in 0..<sub.count {
+    for checkY in 0..<sub[checkX].count {
+      if sub[checkX][checkY] != main[checkX+x][checkY+y] {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+/***** MAIN STARTS HERE *****/
+if let cases = Int(readLine() ?? "0") {
+  var output = [String]()
+
+  // Process Input
+  for _ in 1...cases {
+
+    // Read the case's input matrix
+    var dimensions = String(readLine()!)!
+    var rows = Int(dimensions.components(separatedBy: " ")[0])!
+    var columns = Int(dimensions.components(separatedBy: " ")[1])!
+    var input = [[Int]]()
+    for row in 0..<rows {
+      if let line: String = readLine() {
+        input.append(line.utf8.map({Int($0) - 48;}))
+      }
+    }
+
+    // Read the case's smaller matrix to find
+    dimensions = String(readLine()!)!
+    rows = Int(dimensions.components(separatedBy: " ")[0])!
+    columns = Int(dimensions.components(separatedBy: " ")[1])!
+    var search = [[Int]]()
+    for row in 0..<rows {
+      if let line: String = readLine() {
+        search.append(line.utf8.map({Int($0) - 48;}))
+      }
+    }
+
+    // Process this case
+    var found = "NO"
+    mainLoop: for x in 0...input.count-search.count {
+      for y in 0...input[x].count-search[0].count {
+        if does(thisMatrix: input, containThisOne: search, atThisRow: x, andColumn: y) {
+          found = "YES"
+          break mainLoop
+        }
+      }
+    }
+    output.append(found)
+
+  }
+
+  // Print Output
+  for line in output {
+    print(line)
+  }
+}
+/***** MAIN ENDS HERE *****/
