d8c86b0941077718cd329ee9ee1bb3dd94924ba4
Marwan Alani <marwan@alani.ca>
2017-10-10 15:55:02 -0400

    Added solution for leetCode - Add Two Numbers

---
+++ leetcode/addTwoNumbers.swift
@@ -0,0 +1,39 @@
+//
+//  addTwoNumbers.swift
+//
+//  Practice solution - Marwan Alani - 2017
+//
+//  Check the problem (and run the code) on leetCode @ https://leetcode.com/problems/add-two-numbers/
+//  Note: make sure that you select "Swift" from the top-left language menu of the code editor when testing this code
+//
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func addTwoNumbers(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        var digit = 0, carry = 0, result: ListNode?, itr: ListNode?, l1 = list1, l2 = list2
+        repeat {
+            digit = ((l1?.val) ?? 0) + ((l2?.val) ?? 0) + carry
+            carry = digit / 10
+            if result == nil {
+                result = ListNode(digit % 10)
+                itr = result
+            } else {
+                itr?.next = ListNode(digit % 10)
+                itr = itr?.next
+            }
+            l1 = l1?.next
+            l2 = l2?.next
+        } while (l1 != nil || l2 != nil || carry != 0)
+        return result
+    }
+}
